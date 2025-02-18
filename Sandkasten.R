df6500c7185fb9234f778fe72a9e67b732f23b74
Matthias Lochmann <59730731+Ma-Loma@users.noreply.github.com>
2025-02-18 15:40:33 +0100

    Statistiken zu doppelten Erfassungen

--- Sandkasten.R
+++ Sandkasten.R
@@ -54,3 +54,72 @@ stat2 %>%
         axis.title = element_text(size=txtGr+2))
 ggsave("export/DFLD_Erfassungsraten_files/figure-docx/n1n2.png")
 
+stat2 %>% 
+  filter(DEN=="D") %>% 
+  pivot_wider(names_from = Gewichtsklasse,values_from = c(n1,n2,Erfassungsquote,Leq)) %>% 
+  qflextable()
+
+
+
+praef_data<-praef_data %>%
+  mutate(eventID=paste(StatNr,Fangradius,Lfd,FlugNr,AbAnDatetime,sep="_"))
+
+
+full_data<-full_data %>%
+  mutate(eventID=paste(StatNr,Fangradius,Lfd,FlugNr,AbAnDatetime,sep="_"))
+
+full_data<-praef_Fangradius %>%
+  mutate(praef = Fangradius, .keep = "unused") %>%
+  right_join(full_data)
+
+overlap_df <- full_data %>%
+  group_by(StatNam, Fangradius) %>%  # Group by relevant columns
+#  group_by(StatNam) %>% 
+  arrange(tsAnfDatetime, .by_group = TRUE) %>%  # Ensure correct order
+  mutate(prev_End = lag(tsEndDatetime),
+         prev_ID = lag(eventID))%>%
+  filter(!is.na(prev_End) & tsAnfDatetime < prev_End)  # Overlap condition
+  #select(Event1 = prev_ID, Event2 = eventID, Start1 = lag(tsAnfDatetime), 
+  #       End1 = prev_End, Start2 = tsAnfDatetime, End2 = tsEndDatetime)
+  
+prob_flg <-
+  overlap_df %>%
+  ungroup() %>%
+  select(prev_ID, eventID) %>%
+  unlist() %>%
+  unique()
+
+komisch <-
+  full_data %>%
+  ungroup() %>%
+  filter(eventID %in% prob_flg) %>%
+  arrange(tsAnfDatetime, .by_group = TRUE)
+
+#komisch %>% 
+full_data %>% 
+  mutate(istErkannt=if_else(StatusNr==0,"n1","n2-n1")) %>% 
+  group_by(StatNam,istErkannt,DEN) %>% 
+  summarize(n=n()) %>%
+  pivot_wider(names_from=c(istErkannt,DEN),values_from = n,values_fill = 0)
+  
+full_data %>% 
+  filter(praef==Fangradius) %>% 
+  mutate(istErkannt=if_else(StatusNr==0,"erkannt","unerkannt")) %>%
+  mutate(istGleichzeitig=if_else(eventID %in% prob_flg,"gleichzeitig","einzeln")) %>% 
+  group_by(StatNam,istErkannt,istGleichzeitig,Fangradius) %>% 
+  summarize(n=n()) %>%
+  pivot_wider(names_from=c(istErkannt,istGleichzeitig),values_from = n,values_fill = 0)
+
+komisch %>%  # Ensure correct order
+  filter(praef==Fangradius) %>% 
+  slice_head(n = 8) %>%
+  ggplot(aes(
+    y = factor(Piste),
+    x = tsAnfDatetime,
+    xend = tsEndDatetime
+  )) +
+  geom_segment(aes(yend = factor(Piste),
+                   color = factor(ATD)),
+               linewidth = 2) +
+  labs(x = "Zeiten über Schwelle", y = "AbAnZeit", title = "Ereignisintervalle") +
+  facet_wrap(vars(StatNam),scales="free_x")
